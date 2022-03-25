404243cf9506943d46932514eb0d18ccf7ce8775
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-03-25 16:10:17 +0600

    Start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Products from './components/Products/Products';
 
 function App() {
+  const [guns, setGuns] = useState([]);
+  console.log(guns);
+
+  useEffect(()=>{
+    fetch('https://raw.githubusercontent.com/mir-hussain/guns/main/data.json')
+    .then(res=>res.json())
+    .then(data=>setGuns(data))
+  },[]);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Products></Products>
     </div>
   );
 }
